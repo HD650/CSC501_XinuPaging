2123c8a0e36ddffb45ea924a234a6d07bbad9e16
HD650 <zzhang63@ncsu.edu>
2017-10-28 15:58:37 -0400

    Add the bs get and release api

--- paging/get_bs.c
+++ paging/get_bs.c
@@ -3,11 +3,30 @@
 #include <proc.h>
 #include <paging.h>
 
-int get_bs(bsd_t bs_id, unsigned int npages) {
+extern bs_map_t g_back_store_table[BS_NUM];
+
+int get_bs(bsd_t bs_id, unsigned int npages) 
+{
 
   /* requests a new mapping of npages with ID map_id */
-    kprintf("To be implemented!\n");
-    return npages;
+  kprintf("Get a backing store mapping.\n");
+  STATWORD ps;
+  disable(ps);
+
+  if(g_back_store_table[bs_id].bs_status==BSM_MAPPED)
+  {
+    kprintf("Mapped to a already used mapping.\n");
+    restore(ps);
+    return g_back_store_table[bs_id].bs_npages; 
+  }
+  else
+  {
+    kprintf("Mapped to a new backing store/\n");
+    restore(ps);
+    return g_back_store_table[bs_id].bs_npages;
+  }
+  restore(ps);
+  return SYSERR;
 
 }
 
